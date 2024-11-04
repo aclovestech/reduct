5827a62da51eb8fa1ab75a41921f04ce75d5d42f
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-11-04 06:27:00 -0800

    Adjust flex properties for VoteButtons and Post components; reorder import in Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Post from "../../features/posts/Post";
 import VoteButtons from "../VoteButtons/VoteButtons";
+import Post from "../../features/posts/Post";
 import styles from "./Card.module.css";
 
 export default function Card({ postInfo }) {
