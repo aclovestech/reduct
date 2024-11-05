efda089e3ea3eb6178a9336dc73565465765417e
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-11-04 22:33:01 -0800

    Implement upvote/downvote color change functionality in VoteButtons; update styling in CSS modules

--- src/components/VoteButtons/VoteButtons.js
+++ src/components/VoteButtons/VoteButtons.js
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatVoteCount } from "../../utils/formatter";
 import styles from "./VoteButtons.module.css";
 
 export default function VoteButtons({ score }) {
   const formattedVoteCount = formatVoteCount(score);
+  const [isNeutral, setIsNeutral] = useState(true);
+  const [didUpvote, setDidUpvote] = useState(false);
+  const [didDownvote, setDidDownvote] = useState(false);
+
+  const handleUpvote = () => {
+    if (isNeutral || didDownvote) {
+      setIsNeutral(false);
+      setDidUpvote(true);
+      setDidDownvote(false);
+    } else {
+      setIsNeutral(true);
+      setDidUpvote(false);
+      setDidDownvote(false);
+    }
+  };
+
+  const handleDownvote = () => {
+    if (isNeutral || didUpvote) {
+      setIsNeutral(false);
+      setDidUpvote(false);
+      setDidDownvote(true);
+    } else {
+      setIsNeutral(true);
+      setDidUpvote(false);
+      setDidDownvote(false);
+    }
+  };
+
+  const determineVoteCountColor = () => {
+    if (isNeutral) {
+      return styles.neutral;
+    } else if (didUpvote) {
+      return styles.upvote;
+    } else if (didDownvote) {
+      return styles.downvote;
+    }
+  };
+
   return (
     <div id="vote-buttons-container" className={styles.voteButtons}>
-      <i className="fa-solid fa-arrow-up"></i>
-      <p>{formattedVoteCount}</p>
-      <i className="fa-solid fa-arrow-down"></i>
+      <i
+        className={`${
+          didUpvote ? styles.upvote : styles.neutral
+        } fa-solid fa-arrow-up`}
+        onClick={handleUpvote}
+      ></i>
+      <p className={determineVoteCountColor()}>{formattedVoteCount}</p>
+      <i
+        className={`${
+          didDownvote ? styles.downvote : styles.neutral
+        } fa-solid fa-arrow-down`}
+        onClick={handleDownvote}
+      ></i>
     </div>
   );
 }
